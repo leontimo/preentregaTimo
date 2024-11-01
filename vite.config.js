441b64462e93b9ad6051b4c6b295a9bc4e27f24d
leonardotimo <coderhouseleo@gmail.com>
2024-11-01 18:06:08 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces de red
+    port: process.env.PORT || 3000, // Usa el puerto definido en process.env.PORT o 3000 como respaldo
+  },
 })
